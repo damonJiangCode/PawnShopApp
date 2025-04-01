2ee4963834e9106d8a2b1b8f86abfe8fe98a4ddf
Damon <damonjiangwork@gmail.com>
2025-03-31 22:15:07 -0600

    exposed electronAPI in vite env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+export {};
+
+declare global {
+  interface Window {
+    electronAPI: {
+      searchName: (name: string) => void;
+      searchDatabase: (name: string) => void;
+      getUserInfo: (userID: number) => void;
+    };
+  }
+}
