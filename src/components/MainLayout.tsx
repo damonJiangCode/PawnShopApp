b7ae93d025b19f8b5ffdff381403a1168366ea4f
damon <damonjiangwork@gmail.com>
2025-04-30 23:31:41 -0600

    fix the connection between search and mainlayout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,215 +1,48 @@
 import React, { useState } from "react";
-import {
-  AppBar,
-  Box,
-  Button,
-  CircularProgress,
-  Drawer,
-  IconButton,
-  Stack,
-  Tab,
-  Tabs,
-  TextField,
-  ToggleButton,
-  ToggleButtonGroup,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, IconButton, Tab, Tabs, Toolbar } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
-import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import SwapHorizontalCircleOutlinedIcon from "@mui/icons-material/SwapHorizontalCircleOutlined";
 import SwapHorizontalCircleIcon from "@mui/icons-material/SwapHorizontalCircle";
 import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
 import WorkHistoryOutlinedIcon from "@mui/icons-material/WorkHistoryOutlined";
 import SearchIcon from "@mui/icons-material/Search";
-import ClearIcon from "@mui/icons-material/Clear";
 import TransactionComponent from "./TransactionComponent";
 import HistoryComponent from "./HistoryComponent";
 import ClientComponent from "./ClientComponent";
+import SearchComponent from "./SearchComponent";
 
-const drawerWidth = 250;
+const drawerWidth = 330;
 const collapsedWidth = 0;
 
 const MainLayout: React.FC = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(true);
-  const [searchMode, setSearchMode] = useState("name");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [ticketNumber, setTicketNumber] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [showNoCustomerResults, setShowNoCustomerResults] = useState(false);
-  const [showNoTicketResults, setShowNoTicketResults] = useState(false);
   const [tabIndex, setTabIndex] = useState(0);
 
-  const handleSearch = async () => {
-    if (searchMode === "name") {
-      setIsLoading(true);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      // window.electronAPI.searchClient({ firstName, lastName });
-      setShowNoCustomerResults(true);
-    } else if (searchMode === "ticket") {
-      setIsLoading(true);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      // window.electronAPI.searchTicket({ ticketNumber });
-      setShowNoTicketResults(true);
-    }
-    setIsLoading(false);
-  };
-
-  const handleClear = () => {
-    if (searchMode === "name") {
-      setShowNoCustomerResults(false);
-      setFirstName("");
-      setLastName("");
-    } else if (searchMode === "ticket") {
-      setShowNoTicketResults(false);
-      setTicketNumber("");
-    }
-  };
-
   return (
-    <Box sx={{ display: "flex" }}>
-      {/* left drawer*/}
-      <Box sx={{ display: "flex" }}>
-        <Drawer
-          variant="persistent"
-          anchor="left"
-          open={isDrawerOpen}
-          sx={{
-            width: isDrawerOpen ? drawerWidth : collapsedWidth,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: isDrawerOpen ? drawerWidth : collapsedWidth,
-              p: 2,
-              overflowX: "hidden",
-            },
-          }}
-        >
-          {isDrawerOpen && (
-            <Stack spacing={2}>
-              <Box sx={{ display: "flex", justifyContent: "center" }}>
-                <Typography variant="h5" fontWeight="bold">
-                  Search
-                </Typography>
-              </Box>
+    <Box display="flex">
+      <SearchComponent
+        isDrawerOpen={isDrawerOpen}
+        onDrawerOpenChange={setIsDrawerOpen}
+      />
 
-              {/* Name, Ticket buttons */}
-              <ToggleButtonGroup
-                color="primary"
-                value={searchMode}
-                exclusive
-                onChange={(_event, newValue) => {
-                  if (newValue !== null) setSearchMode(newValue);
-                }}
-                fullWidth
-              >
-                <ToggleButton value="name">
-                  <PersonIcon sx={{ mr: 1 }} />
-                  Name
-                </ToggleButton>
-                <ToggleButton value="ticket">
-                  <ConfirmationNumberIcon sx={{ mr: 1 }} />
-                  Ticket
-                </ToggleButton>
-              </ToggleButtonGroup>
-
-              {/* search input fields */}
-              {searchMode === "name" ? (
-                <>
-                  <TextField
-                    fullWidth
-                    label="Last Name"
-                    size="small"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                  />
-                  <TextField
-                    fullWidth
-                    label="First Name"
-                    size="small"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                  />
-                </>
-              ) : (
-                <TextField
-                  fullWidth
-                  label="Ticket"
-                  size="small"
-                  value={ticketNumber}
-                  onChange={(e) => setTicketNumber(e.target.value)}
-                />
-              )}
-
-              {/* search buttons */}
-              <Stack direction="row" spacing={2}>
-                <Button
-                  fullWidth
-                  variant="contained"
-                  onClick={handleSearch}
-                  disabled={isLoading}
-                  startIcon={
-                    isLoading ? <CircularProgress size={25} /> : <SearchIcon />
-                  }
-                >
-                  {isLoading ? "" : "Search"}
-                </Button>
-                <Button
-                  fullWidth
-                  variant="outlined"
-                  onClick={handleClear}
-                  startIcon={<ClearIcon />}
-                >
-                  Clear
-                </Button>
-              </Stack>
-
-              {showNoCustomerResults && (
-                <Box sx={{ textAlign: "center", py: 2 }}>
-                  <Typography variant="body2" color="text.secondary">
-                    No customers found
-                  </Typography>
-                </Box>
-              )}
-
-              {searchMode == "name" && (
-                <Box sx={{ textAlign: "center", py: 2 }}>
-                  <Button
-                    variant="contained"
-                    sx={{ mt: 2 }}
-                    startIcon={<PersonAddIcon />}
-                  >
-                    Add Customer
-                  </Button>
-                </Box>
-              )}
-
-              {showNoTicketResults && (
-                <Box sx={{ textAlign: "center", py: 2 }}>
-                  <Typography variant="body2" color="text.secondary">
-                    No ticket found
-                  </Typography>
-                </Box>
-              )}
-            </Stack>
-          )}
-        </Drawer>
-      </Box>
-
-      {/* right content */}
       <Box
-        display="flex"
-        justifyContent={isDrawerOpen ? "flex-end" : "center"}
         sx={{
           flexGrow: 1,
-          ml: isDrawerOpen ? `30px` : `${collapsedWidth}px`,
+          position: "relative",
           transition: "margin 0.3s ease-in-out",
         }}
       >
-        <Box sx={{ width: "100%" }}>
+        <Box
+          sx={{
+            position: "fixed",
+            top: 0,
+            right: 0,
+            left: isDrawerOpen ? drawerWidth : collapsedWidth,
+            transition: "left 0.3s ease-in-out",
+            zIndex: 1100,
+          }}
+        >
           <AppBar
             position="static"
             sx={{
@@ -219,14 +52,12 @@ const MainLayout: React.FC = () => {
             }}
           >
             <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-              {/* drawer button */}
               <IconButton
                 onClick={() => setIsDrawerOpen(!isDrawerOpen)}
                 color="inherit"
               >
-                {isDrawerOpen ? <ChevronLeftIcon /> : <SearchIcon />}
+                {!isDrawerOpen && <SearchIcon />}
               </IconButton>
-              {/* tabs */}
               <Tabs
                 value={tabIndex}
                 onChange={(_event, newValue) => setTabIndex(newValue)}
@@ -280,8 +111,14 @@ const MainLayout: React.FC = () => {
               <Box sx={{ width: 48 }} />
             </Toolbar>
           </AppBar>
+        </Box>
 
-          {/* tab contents */}
+        <Box
+          sx={{
+            width: "100%",
+            mt: "64px",
+          }}
+        >
           <Box sx={{ p: 2 }}>
             {tabIndex === 0 && <ClientComponent />}
             {tabIndex === 1 && <TransactionComponent />}
