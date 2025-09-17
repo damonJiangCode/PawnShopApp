6cde78072a3b21ceb8b285992788f245c6048206
damon <damonjiangwork@gmail.com>
2025-09-17 13:52:18 -0600

    id types list created

---
+++ shared/data/IDType.ts
@@ -0,0 +1,21 @@
+export const ID_TYPES = [
+  "Driver's License",
+  "Health Card",
+  "Indian Status Card",
+  "Birth Certificate",
+  "Social Insurance Number Card",
+
+  "Firearms License",
+  "Fishing/Hunting License",
+
+  "Canadian Passport",
+  "Citizenship Card",
+  "Permanent Resident Card",
+
+  "Temporary Resident Permit",
+  "Visitor Record",
+
+  "Military ID",
+
+  "Other",
+];
