7a5ddd5e237e25e22f6e3685999ed51a05b4f8ba
damon <damonjiangwork@gmail.com>
2025-09-24 16:33:05 -0600

    moved id types from frontend to database

--- backend/database/models/idTypesTable.ts
+++ backend/database/models/idTypesTable.ts
@@ -1,23 +1,22 @@
 export const createIdTypesTable = `
   CREATE TABLE IF NOT EXISTS id_types (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE
+    type VARCHAR(50) NOT NULL UNIQUE
   );
 `;
 
 export const insertIdTypes = `
-  INSERT INTO id_types (name) VALUES
-    ('Passport'),
-    ('ID Card'),
-    ('Driver License'),
+  INSERT INTO id_types (type) VALUES
+    ('Driver''s License'),
     ('Health Card'),
-    ('SIN Card'),
+    ('Indian Status Card'),
     ('Birth Certificate'),
+    ('Social Insurance Number Card'),
+    ('Firearms License'),
+    ('Canadian Passport'),
     ('Citizenship Card'),
     ('Permanent Resident Card'),
     ('Military ID'),
-    ('Travel Document'),
-    ('Refugee Travel Document'),
     ('Other')
   ON CONFLICT (name) DO NOTHING;
 `;
